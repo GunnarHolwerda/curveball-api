fddfc776339bde0063a045cb0b17a204329ae62e
Gunnar Holwerda <gunnarholwerda@gmail.com>
2018-08-23 20:09:14 -0700

    Remove heartbeat

--- src/models/base-socket-handler.ts
+++ src/models/base-socket-handler.ts
@@ -1,13 +1,9 @@
 import { Socket } from '../interfaces/socket';
 
 export abstract class BaseSocketHandler {
-
     public register(socket: Socket): void {
-        const heartbeat = setInterval(() => {
-            socket.emit('heartbeat');
-        }, 1000);
         console.log(`User, ${socket.user.userId}, connected to ${this.cachePrefix}`, Date.now());
-        socket.on('disconnect', () => this.disconnect(heartbeat));
+        socket.on('disconnect', this.disconnect.bind(this));
         socket.on('disconnecting', (reason) => {
             console.log(`Disconnecting user ${socket.user.userId} from ${this.cachePrefix}:`, reason);
         });
@@ -18,9 +14,8 @@ export abstract class BaseSocketHandler {
 
     public async destroy(): Promise<any> { }
 
-    protected async disconnect(heartbeat: NodeJS.Timer): Promise<void> {
+    protected async disconnect(): Promise<void> {
         console.table(`User disconnected from ${this.cachePrefix}`);
-        clearInterval(heartbeat);
     }
 
     protected abstract get cachePrefix(): string;
