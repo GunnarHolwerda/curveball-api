de5aa3a2342df1ae303547d57fa5a14eb2bd235d
Gunnar Holwerda <gunnarholwerda@gmail.com>
2018-12-01 11:21:50 -0800

    Track users as they are retrieved

--- src/routes/handlers/users/get-user.ts
+++ src/routes/handlers/users/get-user.ts
@@ -2,6 +2,7 @@
 import * as hapi from 'hapi';
 import * as Boom from 'boom';
 import { UserFactory } from '../../../models/factories/user-factory';
+import { Analytics } from '../../../models/analytics';
 
 export async function getUser(event: hapi.Request): Promise<object> {
     const userId = event.params['userId'];
@@ -10,6 +11,7 @@ export async function getUser(event: hapi.Request): Promise<object> {
         throw Boom.notFound();
     }
     const stats = await user.stats();
+    Analytics.instance.trackUser(user);
 
     return {
         user: user.toResponseObject(),
