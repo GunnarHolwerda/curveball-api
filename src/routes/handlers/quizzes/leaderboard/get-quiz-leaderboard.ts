d485fc86fae9a104273cacfd18d93efb45c3b4ea
Gunnar Holwerda <gunnarholwerda@gmail.com>
2019-02-04 20:06:29 -0800

    Add friends filter to leaderboard endpoint

--- src/routes/handlers/quizzes/leaderboard/get-quiz-leaderboard.ts
+++ src/routes/handlers/quizzes/leaderboard/get-quiz-leaderboard.ts
@@ -1,21 +1,29 @@
 import * as hapi from 'hapi';
 import * as Boom from 'boom';
+import * as Joi from 'joi';
 import { QuizFactory } from '../../../../models/factories/quiz-factory';
 import { Database } from '../../../../models/database';
 import { ANSWER_TABLE_NAME } from '../../../../models/entities/answer';
 import { CHOICES_TABLE_NAME } from '../../../../models/entities/question-choice';
 import { QUESTION_TABLE_NAME } from '../../../../models/entities/question';
 import { UserFactory } from '../../../../models/factories/user-factory';
+import { UserJwtClaims } from '../../../../interfaces/user-jwt-claims';
+import { FRIEND_TABLE_NAME } from '../../../../models/entities/friend';
+
+export const getQuizLeaderboardQueryParams = {
+    filter: Joi.string().optional().allow('friends').description('Filter leaderboard to only friends')
+};
 
 export async function getQuizLeaderboard(event: hapi.Request): Promise<object> {
     const quizId: string = event.params['quizId'];
+    const { filter } = event.query as { filter?: string };
     const quiz = await QuizFactory.load(quizId);
     if (quiz === null) {
         throw Boom.notFound();
     }
 
     const sq = Database.instance.sq;
-    const result = await sq.from({ a: ANSWER_TABLE_NAME })
+    let query = sq.from({ a: ANSWER_TABLE_NAME })
         .join({ c: CHOICES_TABLE_NAME }).on`a.choice_id = c.choice_id`
         .join({ q: QUESTION_TABLE_NAME }).on`q.question_id = a.question_id`
         .where`q.quiz_id = ${quiz.properties.quiz_id}`
@@ -24,6 +32,14 @@ export async function getQuizLeaderboard(event: hapi.Request): Promise<object> {
             user_id: 'a.user_id',
             score: sq.l`SUM(COALESCE(c.score, 0))`
         }).order({ by: 'score', sort: 'desc' });
+
+    if (filter === 'friends') {
+        const { userId: currentUserId } = (event.auth.credentials as UserJwtClaims);
+        query = query.join({ f: FRIEND_TABLE_NAME }).on`f.friend_user_id = a.user_id`
+            .where`f.account_user_id = ${currentUserId}`;
+    }
+
+    const result = await query;
 
     const userScoreMap = result.reduce((carry, r) => {
         carry[r.user_id] = r.score;
